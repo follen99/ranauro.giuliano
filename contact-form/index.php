0420669576dfec179bc9cf434402f9b62bf49592
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-07-19 20:36:30 +0200

    initial commit

---
+++ contact-form/index.php
@@ -0,0 +1,153 @@
+<?php
+	// Start session.
+	session_start();
+	
+	// Set a key, checked in mailer, prevents against spammers trying to hijack the mailer.
+	$security_token = $_SESSION['security_token'] = uniqid(rand());
+	
+	if ( ! isset($_SESSION['formMessage'])) {
+		$_SESSION['formMessage'] = 'Fill in the form below to send me an email.';	
+	}
+	
+	if ( ! isset($_SESSION['formFooter'])) {
+		$_SESSION['formFooter'] = '';
+	}
+	
+	if ( ! isset($_SESSION['form'])) {
+		$_SESSION['form'] = array();
+	}
+	
+	function check($field, $type = '', $value = '') {
+		$string = "";
+		if (isset($_SESSION['form'][$field])) {
+			switch($type) {
+				case 'checkbox':
+					$string = 'checked="checked"';
+					break;
+				case 'radio':
+					if($_SESSION['form'][$field] === $value) {
+						$string = 'checked="checked"';
+					}
+					break;
+				case 'select':
+					if($_SESSION['form'][$field] === $value) {
+						$string = 'selected="selected"';
+					}
+					break;
+				default:
+					$string = $_SESSION['form'][$field];
+			}
+		}
+		return $string;
+	}
+?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+
+	<meta charset="utf-8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="contact" content="contact me" />
+		<meta name="description" content="This page is used to contact me" />
+		<meta name="robots" content="index, follow" />
+		 
+
+	<!-- Meta tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+	<title>Contact me</title>
+	<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/Climate/consolidated.css?rwcache=648412265" />
+		
+	    
+</head>
+
+<!-- This page was created with RapidWeaver from Realmac Software. http://www.realmacsoftware.com -->
+
+<body>
+	<div class="hero" id="hero">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="http://giulianoranauro.com/"><img src="../rw_common/images/SiteIcon.png" width="1024" height="1024" alt="Ranauro Giuliano"/>
+                <span class="navbar-title">Ranauro Giuliano</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../photos/" rel="" class="nav-link">Gallery</a></li><li class="nav-item active"><a href="./" rel="" class="nav-link">Contacts</a></li></ul>
+            </div>
+        </nav>
+
+		<div class="hero-content" id="hero">
+				<h1 class="hero-title">Ranauro Giuliano</h1>
+				<h2 class="hero-slogan">The enter key for your needs</h2>
+		</div>
+
+		<div class="hero-background"></div>
+	</div>
+
+	<main class="content">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-sm-8 main">
+					
+<div class="message-text"><?php echo $_SESSION['formMessage']; unset($_SESSION['formMessage']); ?></div><br />
+
+<form class="rw-contact-form" action="./files/mailer.php" method="post" enctype="multipart/form-data">
+	 <div>
+		<label>Your Name</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element0'); ?>" name="form[element0]" size="40"/><br /><br />
+
+		<label>Your Email</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element1'); ?>" name="form[element1]" size="40"/><br /><br />
+
+		<label>Subject</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element2'); ?>" name="form[element2]" size="40"/><br /><br />
+
+		<label>Message</label> *<br />
+		<textarea class="form-input-field" name="form[element3]" rows="8" cols="38"><?php echo check('element3'); ?></textarea><br /><br />
+
+		<div style="display: none;">
+			<label>Spam Protection: Please don't fill this in:</label>
+			<textarea name="comment" rows="1" cols="1"></textarea>
+		</div>
+		<input type="hidden" name="form_token" value="<?php echo $security_token; ?>" />
+		<input class="form-input-button" type="reset" name="resetButton" value="Reset" />
+		<input class="form-input-button" type="submit" name="submitButton" value="Submit" />
+	</div>
+</form>
+
+<br />
+<div class="form-footer"><?php echo $_SESSION['formFooter']; unset($_SESSION['formFooter']); ?></div><br />
+
+<?php unset($_SESSION['form']); ?>
+
+                </div>
+
+                <div class="col-sm-4 sidebar">
+                    <h4>Other links</h4>
+                    <a href="https://github.com/follen99" target="_blank">GitHub</a><br /><a href="https://www.linkedin.com/in/giuliano-ranauro-b28474190/" target="_blank">LinkedIn</a>
+                    
+                </div>
+			</div>
+		</div>
+
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="footer-content text-center">
+							&copy; 2021 Giuliano Ranauro
+						</div>
+
+						<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../photos/" rel="" class="nav-link">Gallery</a></li><li class="nav-item active"><a href="./" rel="" class="nav-link">Contacts</a></li></ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<script type="text/javascript" src="../rw_common/themes/Climate/js/main.js?rwcache=648412265"></script>
+</body>
+
+</html>
